383310b1e8777f5f35348be1c22637bb368d796f
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-29 17:35:10 -0500

    added f-l-name to seeder file

--- backend/db/seeders/20220825200159-demo-user.js
+++ backend/db/seeders/20220825200159-demo-user.js
@@ -5,16 +5,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        firstName: 'Demo',
+        lastName: 'lition',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Fake',
+        lastName: 'UserUno',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Fake',
+        lastName: 'UserDos',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
