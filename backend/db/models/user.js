38e343f5b26445b1a95e2e008d33ca835197ce1a
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-29 17:17:28 -0500

    added migration file and edited routes for f-l-name

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -28,9 +28,11 @@ module.exports = (sequelize, DataTypes) => {
         return await User.scope('currentUser').findByPk(user.id);
       }
     }
-    static async signup({ username, email, password }) {
+    static async signup({ firstName, lastName, username, email, password}) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
+        firstName,
+        lastName,
         username,
         email,
         hashedPassword
@@ -69,6 +71,14 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
       }
     },
     {
