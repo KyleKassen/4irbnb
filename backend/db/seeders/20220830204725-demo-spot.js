ffffc87545bb4c1a801ec52e5c87734a457c84cd
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-30 16:00:59 -0500

    added seeder file for spots

---
+++ backend/db/seeders/20220830204725-demo-spot.js
@@ -0,0 +1,65 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Spots', [
+      {
+        ownerId: 3,
+        address: "1744 Lone Star Blvd",
+        city: 'Forney',
+        state: 'TX',
+        country: 'United States',
+        lat: 32.684000,
+        lng: -96.405580,
+        name: 'Party Central',
+        description: 'Perfect place for a great time',
+        price: 1500.50
+      },
+      {
+        ownerId: 1,
+        address: "6517 Renewal Rd",
+        city: 'Plano',
+        state: 'TX',
+        country: 'United States',
+        lat: 33.062710,
+        lng: -96.683260,
+        name: 'Party Central 2.0',
+        description: 'Plenty of house to destory. Enjoy.',
+        price: 950
+      },
+      {
+        ownerId: 1,
+        address: "130 Ridge Crest",
+        city: 'College Station',
+        state: 'TX',
+        country: 'United States',
+        lat: 30.562280,
+        lng: -96.316510,
+        name: 'Trailer Park Shenanigans',
+        description: 'Beutiful Mansion',
+        price: 150
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Spots', {
+      address: { [Op.in]: ['1744 Lone Star Blvd', '6517 Renewal Rd', '130 Ridge Crest'] }
+    }, {});
+  }
+};
