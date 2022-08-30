2fdaa7e4cb82c9b4b794a6902086899bf90ce35c
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-30 18:24:15 -0500

    working seeds for review and spot images

---
+++ backend/db/seeders/20220830230612-demo-review.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const reviewData = [
+  {
+    spotId: 1,
+    userId: 1,
+    review: 'Beautiful home love it',
+    stars: 5
+  },
+  {
+    spotId: 2,
+    userId: 2,
+    review: 'Absolute beut of a home.',
+    stars: 4
+  },
+  {
+    spotId: 2,
+    userId: 3,
+    review: 'Worst Home there is',
+    stars: 1
+  }
+]
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Reviews', reviewData, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    const { Op } = require('sequelize');
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Reviews', { [Op.or]: reviewData }, {});
+  }
+};
