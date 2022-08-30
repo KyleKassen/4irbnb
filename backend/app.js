88e049f6d95615708021ec18d470e34091138ccd
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-30 15:22:34 -0500

    minor changes to error formatting

--- backend/app.js
+++ backend/app.js
@@ -68,12 +68,18 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     console.error(err);
+    const response = {};
+    response.message = err.message;
+    response.statusCode = err.status;
+    response.errors = err.errors;
+    if (!isProduction) response.stack = err.stack;
     res.json({
         // title: err.title || 'Server Error',
-        message: err.message,
-        statusCode: err.status,
-        errors: err.errors,
-        stack: isProduction ? null : err.stack
+        // message: err.message,
+        // statusCode: err.status,
+        // errors: err.errors,
+        // stack: isProduction ? null : err.stack
+        ...response
     });
 });
 
