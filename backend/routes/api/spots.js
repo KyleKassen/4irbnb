ee503b6651f9bfe56f2e541f1da8f65e57707ac9
Kyle Kassen <kyle.kassen@gmail.com>
2022-08-30 19:42:09 -0500

    get all spots is working

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,16 +1,52 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot, Review, sequelize, SpotImage, Sequelize } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+
+const { Op } = require('sequelize');
 
 const router = express.Router();
 
 
 // Get all Spots
-router.get('/', async(req, res, next) => {
-    const spots = await Spot.findAll();
+router.get('/', async (req, res, next) => {
+
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: SpotImage,
+                as: 'previewImage',
+                attributes: ['url'],
+                where: {
+                    preview: true
+                },
+                required: false,
+            }
+        ],
+        attributes: {
+            include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgRating"]]
+        },
+        group: 'Spot.id'
+    });
+
+    for(let i = 0; i < spots.length; i++) {
+        let newSpot = spots[i].toJSON()
+        // console.log(newSpot)
+        if (newSpot.previewImage[0]) {
+            let url = ''
+            console.log(newSpot.previewImage[0].url)
+            url = newSpot.previewImage[0].url
+            newSpot.previewImage = url;
+        }
+        
+        spots[i] = newSpot
+    }
 
     return res.json(spots)
 })
