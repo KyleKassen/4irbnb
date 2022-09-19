7ac55080e6f25a41d17181f99e0431c57dda1103
Kyle Kassen <kyle.kassen@gmail.com>
2022-09-19 15:52:09 -0500

    nav button styled

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 import Navigation from "../Navigation";
-import './Header.css';
+import "./Header.css";
 
-
-function Header({isLoaded}) {
-
-    return (
-        <div className="main_header">
-            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png" />
-            <Navigation isLoaded={isLoaded} />
-        </div>
-    )
+function Header({ isLoaded }) {
+  return (
+    <div className="main_header_flex_wrapper">
+      <div className="main_header">
+        <img
+          className="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+        />
+        <Navigation isLoaded={isLoaded} />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,29 +1,30 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {getAllSpots} from '../../store/spot';
-import SpotCard from '../SpotCard';
-import './Spots.css';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSpots } from "../../store/spot";
+import SpotCard from "../SpotCard";
+import "./Spots.css";
 
 function Spots() {
-    const dispatch = useDispatch();
-    // const order = useSelector(state => state.spots.allSpots.order)
-    const allSpots = useSelector(state => state.spots.allSpots)
+  const dispatch = useDispatch();
+  // const order = useSelector(state => state.spots.allSpots.order)
+  const allSpots = useSelector((state) => state.spots.allSpots);
 
-    useEffect(() => {
-        console.log('Get all spots running in SPOTS useEffect')
-        dispatch(getAllSpots());
-    }, [dispatch]);
+  useEffect(() => {
+    console.log("Get all spots running in SPOTS useEffect");
+    dispatch(getAllSpots());
+  }, [dispatch]);
 
-    console.log('allSpots.order is : ',allSpots.order)
-    return (
-        <div className="spots_container">
-            {allSpots.order.map(spotId => {
-                console.log('allSpots[spotId] : ',allSpots[spotId])
-                return <SpotCard spot={allSpots[spotId]} />
-            })}
-        </div>
-    )
+  console.log("allSpots.order is : ", allSpots.order);
+  return (
+    <div className="spots_container_flex_wrapper">
+      <div className="spots_container">
+        {allSpots.order.map((spotId) => {
+          console.log("allSpots[spotId] : ", allSpots[spotId]);
+          return <SpotCard spot={allSpots[spotId]} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Spots;
