d85510a5445be52cfbd0270df6f635285a30a1bf
Kyle Kassen <kyle.kassen@gmail.com>
2022-09-19 16:44:17 -0500

    logo redirects user

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Navigation from "../Navigation";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header({ isLoaded }) {
   return (
     <div className="main_header_flex_wrapper">
       <div className="main_header">
-        <img
-          className="logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
-        />
+        <Link to="/">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+          />
+        </Link>
         <Navigation isLoaded={isLoaded} />
       </div>
     </div>
