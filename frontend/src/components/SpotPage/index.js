e7268da0edd6c3c20f7ae9ea1ee5d08ccadf1d62
Kyle Kassen <kyle.kassen@gmail.com>
2022-09-20 17:03:56 -0500

    format spot page gallery

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -9,6 +9,20 @@ function SpotPage() {
   const dispatch = useDispatch();
 
   const spot = useSelector((state) => state.spots.singleSpot);
+  const blankImg = [];
+  if (spot) {
+    if (spot.SpotImages.length < 5) {
+      const numImg = spot.SpotImages.length;
+      for (let i = 0; i < 5 - numImg; i++) {
+        blankImg.push(
+          <img
+            className="spot_page_gallery_image"
+            src="https://archive.org/download/no-photo-available/no-photo-available.png"
+          />
+        );
+      }
+    }
+  }
 
   useEffect(() => {
     dispatch(getOneSpot(id));
@@ -19,14 +33,27 @@ function SpotPage() {
       {spot && (
         <div className="spot_page_flex_container">
           <div className="spot_page_container">
-            <p>{spot.description}</p>
-            <p>
+            <p className="spot_page_description">{spot.description}</p>
+            <p className="spot_page_under_desc">
               <i class="fa-sharp fa-solid fa-star"></i>
               {spot.avgRating} · {spot.numReviews} reviews · Superhost ·{" "}
               {spot.city}, {spot.state}, {spot.country}
             </p>
             <div className="spot_image_gallery_wrapper">
-                
+              {spot.SpotImages.map((spotObj) => {
+                if (spotObj.preview) {
+                  return (
+                    <img
+                      className="spot_page_gallery_preview_image"
+                      src={spotObj.url}
+                    />
+                  );
+                }
+                return (
+                  <img className="spot_page_gallery_image" src={spotObj.url} />
+                );
+              })}
+              {blankImg.length && blankImg.map((img) => img)}
             </div>
           </div>
         </div>
