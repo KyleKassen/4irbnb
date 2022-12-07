d49501cb73313884d052f41f108fcb37ec2f94c1
Kyle Kassen <kyle.kassen@gmail.com>
2022-12-07 14:56:00 -0600

    search feature layout established

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navigation from "../Navigation";
 import { Link } from "react-router-dom";
 import CreateSpotModal from "../CreateSpotModal";
@@ -6,19 +6,36 @@ import "./Header.css";
 
 function Header({ isLoaded }) {
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
 
   return (
     <div className="main_header_flex_wrapper">
       <div className="main_header">
         <Link to="/">
-          <img
-            className="logo"
-            src="https://i.ibb.co/mB0RYJx/4irbnb-1.png"
-          />
+          <img className="logo" src="https://i.ibb.co/mB0RYJx/4irbnb-1.png" />
         </Link>
+        <div className="header-search-container">
+          <div className="search-input-container">
+          <input
+            className="search-input"
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="Start your search"
+          />
+          <button><i></i></button>
+          </div>
+        </div>
         <div className="main_header_right_side">
-          <CreateSpotModal showLoginModal={showLoginModal} setShowLoginModal={setShowLoginModal}/>
-          <Navigation isLoaded={isLoaded} showModal={showLoginModal} setShowModal={setShowLoginModal}/>
+          <CreateSpotModal
+            showLoginModal={showLoginModal}
+            setShowLoginModal={setShowLoginModal}
+          />
+          <Navigation
+            isLoaded={isLoaded}
+            showModal={showLoginModal}
+            setShowModal={setShowLoginModal}
+          />
         </div>
       </div>
     </div>
